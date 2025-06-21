83941cfa29f62380933d2125eb1780b0242e4f61
Nguyenntse <NguyenNTSE180106@fpt.edu.vn>
2025-06-21 14:05:35 +0700

    feat: implement [get] GetAll api/v1/patients

---
+++ FertilityCare.WebApi/Controllers/PatientController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using FertilityCare.Infrastructure.Services;
+using FertilityCare.Shared.Exceptions;
+using FertilityCare.UseCase.DTOs.Patients;
+using FertilityCare.UseCase.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FertilityCare.WebAPI.Controllers
+{
+    [Route("api/v1/patients")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly IPatientService _patientService;
+
+        private readonly IPatientSecretService _patientSecretService;
+
+        public PatientController(IPatientService patientService, IPatientSecretService patientSecretService)
+        {
+            _patientService = patientService;
+            _patientSecretService = patientSecretService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse<IEnumerable<PatientDTO>>>> GetAllAynsc()
+        {
+            try
+            {
+                var result = await _patientService.FindAllAsync();
+                return Ok(new ApiResponse<IEnumerable<PatientDTO>>
+                {
+                    StatusCode = 200,
+                    Message = "",
+                    Data = result,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ApiResponse<string>
+                {
+                    StatusCode = 400,
+                    Message = e.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+
+        }
+
+        
+    }
+}
