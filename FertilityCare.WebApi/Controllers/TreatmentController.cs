4473b5a09496e3ccc85655ea760b915b68a6e56f
Nguyenntse <NguyenNTSE180106@fpt.edu.vn>
2025-06-21 13:57:46 +0700

    feat: implement [get] GetAll api/v1/treatments

---
+++ FertilityCare.WebApi/Controllers/TreatmentController.cs
@@ -0,0 +1,47 @@
+﻿using FertilityCare.UseCase.DTOs.TreatmentServices;
+using FertilityCare.UseCase.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FertilityCare.WebAPI.Controllers
+{
+    [Route("api/v1/treatments")]
+    [ApiController]
+    public class TreatmentController : ControllerBase
+    {
+        private readonly IPublicTreatmentService _publicTreatmentService;
+        public TreatmentController(IPublicTreatmentService publicTreatmentService)
+        {
+            _publicTreatmentService = publicTreatmentService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse<IEnumerable<TreatmentServiceDTO>>>> GetAll()
+        {
+            try
+            {
+                var result = await _publicTreatmentService.GetAllAsync();
+              
+                return Ok(new ApiResponse<IEnumerable<TreatmentServiceDTO>>
+                {
+                    StatusCode = 200,
+                    Message = "hihiih",
+                    Data = result,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ApiResponse<IEnumerable<TreatmentServiceDTO>>
+                {
+                    StatusCode = 400,
+                    Message = ex.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+        }
+
+       
+    }
+}
