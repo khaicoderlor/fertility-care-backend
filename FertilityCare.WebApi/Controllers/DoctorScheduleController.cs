81eb5b55f1b50e678b41b71c8cbbdb3d27b02f89
thanghsse <thanghsse180107@fpt.edu.vn>
2025-06-21 11:16:38 +0700

    feat: implement [get] FindAllSchedulesAsync api/v1/doctor-schedules

---
+++ FertilityCare.WebApi/Controllers/DoctorScheduleController.cs
@@ -0,0 +1,49 @@
+﻿using Fertilitycare.Share.Comon;
+using Fertilitycare.Share.Pagination;
+using FertilityCare.Shared.Exceptions;
+using FertilityCare.UseCase.DTOs.DoctorSchedules;
+using FertilityCare.UseCase.DTOs.Slots;
+using FertilityCare.UseCase.DTOs.UserProfiles;
+using FertilityCare.UseCase.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FertilityCare.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/doctor-schedules")]
+    public class DoctorScheduleController : ControllerBase
+    {
+        private readonly IDoctorScheduleService _doctorScheduleService;
+
+        public DoctorScheduleController(IDoctorScheduleService doctorScheduleService)
+        {
+            _doctorScheduleService = doctorScheduleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse<IEnumerable<DoctorScheduleDTO>>>> FindAllSchedulesAsync()
+        {
+            try
+            {
+                var result = await _doctorScheduleService.FindAllSchedulesAsync();
+                return Ok(new ApiResponse<IEnumerable<DoctorScheduleDTO>>
+                {
+                    StatusCode = 200,
+                    Message = "Schedules fetched successfully.",
+                    Data = result,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new ApiResponse<object>
+                {
+                    StatusCode = 500,
+                    Message = e.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+        }
+    }
+}
