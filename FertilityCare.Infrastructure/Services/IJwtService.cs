360f5000a6a23037ff31c5bff162d3c1fede32cf
khaicoderlor <khainqse180154@fpt.edu.vn>
2025-06-21 16:09:09 +0700

    feat: implement JwtService

---
+++ FertilityCare.Infrastructure/Services/IJwtService.cs
@@ -0,0 +1,119 @@
+﻿using FertilityCare.Domain.Entities;
+using FertilityCare.Infrastructure.Configurations;
+using FertilityCare.Infrastructure.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FertilityCare.Infrastructure.Services
+{
+    public interface IJwtService
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
+
+        bool ValidateToken(string token);
+
+    }
+
+    public class JwtService : IJwtService
+    {
+        private readonly JwtConfiguration _jwtConfig;
+
+        public JwtService(IOptions<JwtConfiguration> jwtConfiguration)
+        {
+            _jwtConfig = jwtConfiguration.Value;
+        }
+
+
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfig.SecretKey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtConfig.Issuer,
+                audience: _jwtConfig.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtConfig.ExpirationInMinutes),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateRefreshToken()
+        { 
+            var randomNumber = new byte[64];
+            using var r = RandomNumberGenerator.Create();
+            r.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        public ClaimsPrincipal? GetPrincipalFromExpiredToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfig.SecretKey)),
+                ValidateLifetime = false
+            };
+
+            var handler = new JwtSecurityTokenHandler();
+
+            try
+            {
+                var principal = handler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+                if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+                    !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.OrdinalIgnoreCase))
+                {
+                    throw new SecurityTokenException("Invalid token");
+                }
+
+                return principal;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool ValidateToken(string token)
+        {
+            try
+            {
+                var hanlder = new JwtSecurityTokenHandler();
+
+                hanlder.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfig.SecretKey)),
+                    ValidIssuer = _jwtConfig.Issuer,
+                    ValidAudience = _jwtConfig.Audience,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken securityToken);
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
