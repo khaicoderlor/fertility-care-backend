af1ec35f1cbc250af2be3191db009191d2781a0c
Nguyenntse <NguyenNTSE180106@fpt.edu.vn>
2025-06-21 14:06:06 +0700

    feat: implement [get] GetPatientById api/v1/patients?patientId=

--- FertilityCare.WebApi/Controllers/PatientController.cs
+++ FertilityCare.WebApi/Controllers/PatientController.cs
@@ -50,6 +50,73 @@ namespace FertilityCare.WebAPI.Controllers
 
         }
 
-        
+        [HttpGet("{patientId}")]
+        public async Task<ActionResult<ApiResponse<PatientDTO>>> GetPatientByIdAsync([FromRoute]string patientId)
+        {
+            try
+            {
+                var result = await _patientService.FindPatientByPatientIdAsync(patientId);
+                return Ok(new ApiResponse<PatientDTO>
+                {
+                    StatusCode = 200,
+                    Message = "",
+                    Data = result,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ApiResponse<string>
+                {
+                    StatusCode = 400,
+                    Message = e.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+        }
+
+        [HttpPost("me")]
+        public async Task<ActionResult<ApiResponse<PatientSecretInfo>>> GetPatientSecretInfo()
+        {
+            var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            try
+            {
+                var patientInfo = await _patientSecretService.GetPatientByUserIdAsync(id);
+                return Ok(new ApiResponse<PatientSecretInfo>
+                {
+                    StatusCode = 200,
+                    Message = "",
+                    Data = patientInfo,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch (NotFoundException)
+            {
+                return Ok(new ApiResponse<PatientSecretInfo>
+                {
+                    StatusCode = 200,
+                    Message = "",
+                    Data = new PatientSecretInfo
+                    {
+                        PatientId = null,
+                        UserProfileId = null,
+                        OrderIds = null,
+                    },
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch(Exception e)
+            {
+                return BadRequest(new ApiResponse<string>
+                {
+                    StatusCode = 400,
+                    Message = e.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+           
+        }
     }
 }
