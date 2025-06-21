da0dde968b4cd54f16526ab5c34c4091b05bf27a
khaicoderlor <khainqse180154@fpt.edu.vn>
2025-06-21 16:15:34 +0700

    feat: implement [post] Login api/v1/auth

---
+++ FertilityCare.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using FertilityCare.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FertilityCare.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/auth")]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResult>> Login([FromBody] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authService.LoginAsync(request);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(new { messsage = result.ErrorMessage });
+            }
+
+            return Ok(result.Data);
+        }
+    }
+}
