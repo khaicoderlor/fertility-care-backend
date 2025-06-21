ae6f95a287583140ea320cb199971d5cb4de78c8
khaicoderlor <khainqse180154@fpt.edu.vn>
2025-06-21 15:47:31 +0700

    feat: implement UserProfileRepository, SlotRepository

---
+++ FertilityCare.UseCase/Interfaces/Repositories/ISlotRepository.cs
@@ -0,0 +1,14 @@
+﻿using FertilityCare.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FertilityCare.UseCase.Interfaces.Repositories
+{
+    public interface ISlotRepository : IBaseRepository<Slot, long>
+    {
+        Task<Slot?> FindSlotAsync(TimeOnly startTime, TimeOnly endTime);
+    }
+}
---
+++ FertilityCare.UseCase/Interfaces/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,17 @@
+﻿using FertilityCare.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FertilityCare.UseCase.Interfaces.Repositories
+{
+    public interface IUserProfileRepository : IBaseRepository<UserProfile, Guid>
+    {
+
+        public Task SaveChangeAsync();
+
+        public Task<UserProfile?> FindByUserIdAsync(string userId);
+    }
+}
