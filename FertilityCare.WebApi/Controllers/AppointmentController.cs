219e8e9bb9245f0ca751714b42b14065cc899a06
khaicoderlor <khainqse180154@fpt.edu.vn>
2025-06-21 16:05:02 +0700

    feat: implement [post] CreateAppointmentByStepId api/v1/appointments

--- FertilityCare.WebApi/Controllers/AppointmentController.cs
+++ FertilityCare.WebApi/Controllers/AppointmentController.cs
@@ -55,6 +55,53 @@ namespace FertilityCare.WebApi.Controllers
             }
         }
 
+        [HttpPost]
+        [Route("{orderId}")]
+        public async Task<ActionResult<ApiResponse<AppointmentDTO>>> CreateAppointmentByStepId([FromRoute] string orderId, [FromBody] CreateAppointmentDailyRequestDTO request)
+        {
+            try
+            {
+                var result = await _appointmentService.PlaceAppointmentByStepIdAsync(Guid.Parse(orderId), request);
+                return Ok(new ApiResponse<AppointmentDTO>
+                {
+                    StatusCode = 200,
+                    Message = "Appointment created successfully",
+                    Data = result,
+                    ResponsedAt = DateTime.UtcNow
+                });
+            }
+            catch (NotFoundException e)
+            {
+                return NotFound(new ApiResponse<object>
+                {
+                    StatusCode = 404,
+                    Message = e.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.UtcNow
+                });
+            }
+            catch (AppointmentSlotLimitExceededException e)
+            {
+                return BadRequest(new ApiResponse<object>
+                {
+                    StatusCode = 400,
+                    Message = e.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.UtcNow
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ApiResponse<object>
+                {
+                    StatusCode = 400,
+                    Message = ex.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.UtcNow
+                });
+
+            }
+        }
 
 
     }
