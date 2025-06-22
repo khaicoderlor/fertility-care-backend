f92bed4ba9bc80a34c05344d0980a4e29345f3da
Nguyenntse <NguyenNTSE180106@fpt.edu.vn>
2025-06-22 17:15:44 +0700

    feat: implement [post] CreateEmbryoTransferAsync api/v1/embryotransfer?isFrozen=

--- FertilityCare.WebApi/Controllers/EmbryoTransferController.cs
+++ FertilityCare.WebApi/Controllers/EmbryoTransferController.cs
@@ -18,7 +18,41 @@ namespace FertilityCare.WebApi.Controllers
         {
             _embryoTransferService = embryoTransferService;
         }
-        
-        
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse<EmbryoTransferDTO>>> CreateEmbryoTransferAsync([FromBody] CreateEmbryoTransferRequestDTO request, [FromQuery] bool isFrozen)
+        {
+            try
+            {
+                var result = await _embryoTransferService.CreateEmbryoTransferAsync(request, isFrozen);
+                return Ok(new ApiResponse<EmbryoTransferDTO>
+                {
+                    StatusCode = 200,
+                    Message = "Embryo Transfer successfully.",
+                    Data = result,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(new ApiResponse<object>
+                {
+                    StatusCode = 404,
+                    Message = "Embryo Transfer Not Found.",
+                    Data = null,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ApiResponse<EmbryoTransferDTO>
+                {
+                    StatusCode = 400,
+                    Message = ex.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+        }
+
     }      
 }
