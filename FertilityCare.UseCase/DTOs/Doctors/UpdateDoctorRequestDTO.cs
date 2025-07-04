598d90669359aa40a36beddd35d25922913c88c4
thanghsse <thanghsse180107@fpt.edu.vn>
2025-07-04 12:53:49 +0700

    feat: add UpdateDoctorRequestDTO

---
+++ FertilityCare.UseCase/DTOs/Doctors/UpdateDoctorRequestDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FertilityCare.UseCase.DTOs.Doctors
+{
+    public class UpdateDoctorRequestDTO
+    {
+        public string Degree { get; set; }
+        public string Specialization { get; set; }
+        public int YearsOfExperience { get; set; }
+        public string Biography { get; set; }
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; } // ISO 8601 (yyyy-MM-dd)
+        public string Address { get; set; }
+    }
+}
