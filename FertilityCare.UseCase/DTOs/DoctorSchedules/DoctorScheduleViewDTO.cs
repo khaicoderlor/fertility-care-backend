b5f84c0fb74ed00ec67c100eab24bf1367882798
thanghsse <thanghsse180107@fpt.edu.vn>
2025-07-04 10:45:52 +0700

    feat: add DoctorScheduleViewDTO

---
+++ FertilityCare.UseCase/DTOs/DoctorSchedules/DoctorScheduleViewDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FertilityCare.UseCase.DTOs.DoctorSchedules
+{
+    public class DoctorScheduleViewDTO
+    {
+        public DateOnly WorkDate { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
