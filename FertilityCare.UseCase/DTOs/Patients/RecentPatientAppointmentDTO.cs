916bae885fc529f7e97a78609fd6a43d343b1c15
thanghsse <thanghsse180107@fpt.edu.vn>
2025-07-04 10:46:34 +0700

    feat: add RecentPatientAppointmentDTO

---
+++ FertilityCare.UseCase/DTOs/Patients/RecentPatientAppointmentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FertilityCare.UseCase.DTOs.Patients
+{
+    public class RecentPatientAppointmentDTO
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string TreatmentServiceName { get; set; }
+        public DateOnly LastAppointmentDate { get; set; }
+        public DateOnly? NextAppointmentDate { get; set; }
+    }
+}
