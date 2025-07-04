c615e4a059a057b6e7b3b939dd1ec4eeb2197de5
khaicoderlor <khainqse180154@fpt.edu.vn>
2025-07-04 18:03:02 +0700

    ad:: adding field to return in DoctorScheduleViewDTO api/v1/doctors/doctorId/weekly-schedules

--- FertilityCare.UseCase/DTOs/DoctorSchedules/DoctorScheduleViewDTO.cs
+++ FertilityCare.UseCase/DTOs/DoctorSchedules/DoctorScheduleViewDTO.cs
@@ -9,11 +9,22 @@ namespace FertilityCare.UseCase.DTOs.DoctorSchedules
     public class DoctorScheduleViewDTO
     {
         public string WorkDate { get; set; }
+
         public string StartTime { get; set; }
+
         public string EndTime { get; set; }
 
         public string FirstName { get; set; }
+
         public string? MiddleName { get; set; }
+
         public string LastName { get; set; }
+
+        public string ShiftType { get; set; }
+
+        public string DoctorId { get; set; }
+
+        public long ScheduleId { get; set; }
+
     }
 }
